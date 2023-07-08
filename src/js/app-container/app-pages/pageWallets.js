5aca512227d68c6f9f1add4f426f5966c4dd6683
Sahil <heysahilbro@gmail.com>
2023-07-08 18:47:40 +0530

    Delete src directory

--- src/js/app-container/app-pages/pageWallets.js
+++
@@ -1,7 +0,0 @@
-import { appendHTML, createHTML } from '../../helper.js';
-
-export function pageWallets(){
-    const pageWallets = createHTML("div", {class: "page page-wallets"}, "Page Wallets");
-
-    return pageWallets
-}
