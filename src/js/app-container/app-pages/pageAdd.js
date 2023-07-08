95bf3b54deb7fd65114a301521269c1a12c464e7
Sahil <heysahilbro@gmail.com>
2023-07-08 08:58:28 +0530

    Delete src directory

--- src/js/app-container/app-pages/pageAdd.js
+++
@@ -1,7 +0,0 @@
-import { appendHTML, createHTML } from '../../helper.js';
-
-export function pageAdd(){
-    const pageAdd = createHTML("div", {class: "page page-add"}, "Page Add");
-
-    return pageAdd
-}
