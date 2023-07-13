a355ac0c360ddfad478fa3c75bf4a447cdeed16d
Sahil <heysahilbro@gmail.com>
2023-07-13 11:57:24 +0530

    Delete src directory

--- src/js/app-container/app-pages/pageReports.js
+++
@@ -1,7 +0,0 @@
-import { appendHTML, createHTML } from '../../helper.js';
-
-export function pageReports(){
-    const pageReports = createHTML("div", {class: "page page-reports"}, "Page Reports");
-
-    return pageReports
-}
--- src/js/app-container/app-pages/pageSettings.js
+++
@@ -1,7 +0,0 @@
-import { appendHTML, createHTML } from '../../helper.js';
-
-export function pageSettings(){
-    const pageSettings = createHTML("div", {class: "page page-settings"}, "Page settings");
-
-    return pageSettings
-}
