4270329eaaca8898a263110fe414235e3897545d
Sahil <heysahilbro@gmail.com>
2023-07-19 18:52:58 +0530

    Add files via upload

---
+++ src/js/main.js
@@ -0,0 +1,45 @@
+import { dbAppSettings } from "./db.js";
+import { updateAppColors, updateAppUserCurrency } from "./helper.js";
+
+// App
+export const mainHTML = document.querySelector(".main");
+export const appHTML = mainHTML.querySelector(".app");
+
+// Dialog Box
+export const dialogBoxHTML = document.querySelector(".dialog-box");
+export const dialogBoxWrapperHTML = dialogBoxHTML.querySelector(
+  ".dialog-box-wrapper"
+);
+
+// Detect user's theme preference
+// if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+//   dbAppSettings.darkMode = true;
+//   updateAppColors();
+// }
+
+// const colorSchemeChangeHandler = event => {
+//   dbAppSettings.darkMode = event.matches;
+//   updateAppColors();
+// };
+
+// const colorSchemeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+// colorSchemeMediaQuery.addEventListener('change', colorSchemeChangeHandler);
+
+// Add CSS variables
+updateAppUserCurrency();
+updateAppColors();
+
+// Render Sign Up Screen or App Container Screen
+try {
+  if (true) {
+    import("./screenApp.js").then(({ screenApp }) => {
+      screenApp();
+    });
+  } else {
+    import("./screenSignup.js").then(({ screenSignup }) => {
+      screenSignup();
+    });
+  }
+} catch (error) {
+  alert(error);
+}
